cce3ca5c40136aa188b4e7bd01d61a2c56ca04aa
Nick <69305722+Bac0nEater@users.noreply.github.com>
2023-08-26 12:57:04 +1200

    feat: add goal positions

--- format.py
+++ format.py
@@ -22,9 +22,32 @@ def create_centerline(city):
 
     df.to_csv(f'./{city}/center_line_third.csv', index=False, header=False)
     print(df)
+
+def create_goalpositions(city):
+    city = city.replace(' ', '_')
+    if city.startswith('.'):
+        return
     
+    if city == 'Mexico_City':
+        df = pd.read_csv(f'{city}/MexicoCity_centerline.csv', encoding='latin-1')
+    else:
+        df = pd.read_csv(f'{city}/{city}_centerline.csv')
+
+    # print(df)
+
+    df = df.drop(columns=[" w_tr_right_m", ' w_tr_left_m'], axis=1)
+
+    df = df.iloc[::3, :]
+
+    df['goal_position'] = df.apply(lambda row: (row['# x_m'], row[' y_m']), axis=1)
+    df = df['goal_position']
+    # df = pd.DataFrame.from_dict({'goal_positions': df['goal_position'].values})
+    print(df)
+    df.to_json(f'./{city}/goal_positions.json', orient='records', indent=0)
+
+
 directories = os.listdir('./')
 
 for directory in directories:
     if os.path.isdir(directory):
-        create_centerline(directory)
+        create_goalpositions(directory)
